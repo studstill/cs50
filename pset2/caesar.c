9a572a7b25f9b86ac8975f18f41273e45ffacd02
Jason Studstill <standstill64@gmail.com>
2015-03-11 17:07:05 -0400

    Streamline starting conditions and prevent core dump

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -4,40 +4,41 @@
 
 int main(int argc, string argv[])
 {
-    printf("%i\n", argc);
-    
     if (argv[1] != NULL)
     {
-        printf("%c\n", argv[1][0]);
-        printf("%s\n", argv[1]);
-    
-    
-        // Convert string inside argv[1] to an int
-        int k = atoi(argv[1]);
-        printf("%i\n", k);
+        // Convert command-line input from string to int.  Variabe key gets resulting int
+        int key = atoi(argv[1]);
+        
+        // Declare variables to check boolean conditions
+        int only_two_arguements = 0;
+        int is_positive_int = 0;
 
-        if (argc != 2)
+        // Check that there are only 2 comand-line arguements
+        if (argc == 2)
+            only_two_arguements = 1;
+
+        // Check for positive int
+        if (key > 0)
+            is_positive_int = 1;
+
+       // Run program if conditions are true
+        if (only_two_arguements && is_positive_int)
         {
-            printf("Please run the program again and include a positive int as your " 
-            "second command-line arguement\n");
-            return 1;
+            printf("woo hoo\n");
         }
-        else if (argv[1][0] > '9')
-        {
-            printf("Please run the program again and include a positive int as your " 
-            "second command-line arguement\n");
-            return 1;
-        }
-        else if (argv[1][0] < '1')
-        {
-            printf("Please run the program again and include a positive int as your " 
-            "second command-line arguement\n");
-            return 1;  
-        }
+        
+ 
         else
-        { 
-            printf("You did it!\n");
-            return 0;
+        {
+            printf("Please run the program again and include a positive int as your " 
+            "second command-line arguement\n");
+            return 1;
         }
+    }
+    else
+    {
+        printf("Please run the program again and include a positive int as your " 
+        "second command-line arguement\n");
+        return 1;
     }
 }
