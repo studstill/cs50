e50a6e20d768a8568060d009106e69aebe22d0bd
Jason Studstill <standstill64@gmail.com>
2015-03-12 01:42:09 -0400

    Complete hello.c, update to comply with check50

--- pset1/hello.c
+++ pset1/hello.c
@@ -1,12 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-//This is my first C program in cs50!
+// This is my first C program in cs50!
 
 int main(void)
 {
-    printf("State your name: ");
-    string name = GetString();  
-    printf("hello, %s\n", name);
+    // Print hello, world
+    printf("hello, world\n");
     
 }
